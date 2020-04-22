08fa5b2723113e3af09e21c78ade949cb3da7dcd
Malintha1996 <malintha1996@gmail.com>
2020-04-22 22:16:12 +0530

    Add initial implementation

---
+++ README.md
@@ -0,0 +1,1 @@
+## Data store for disease related data
---
+++ src/main/java/io/datasearch/denguestore/DengueStore.java
@@ -0,0 +1,8 @@
+package io.datasearch.denguestore;
+
+/**
+ * DengueStore is a spatio-temporal data store for disease related data.
+ */
+public class DengueStore {
+
+}
