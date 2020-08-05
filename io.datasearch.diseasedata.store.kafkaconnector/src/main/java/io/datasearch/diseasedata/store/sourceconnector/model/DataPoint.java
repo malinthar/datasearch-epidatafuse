314c44b6fa41ad2461a88cab04891c8f66f0e046
Malintha1996 <malintha1996@gmail.com>
2020-08-05 21:57:00 +0530

    Refactor for demonstration-1

--- io.datasearch.diseasedata.store.kafkaconnector/src/main/java/io/datasearch/diseasedata/store/sourceconnector/model/DataPoint.java
+++ io.datasearch.diseasedata.store.kafkaconnector/src/main/java/io/datasearch/diseasedata/store/sourceconnector/model/DataPoint.java
@@ -10,26 +10,74 @@ import org.apache.kafka.connect.data.Struct;
  */
 public class DataPoint {
     /**
-     * Schema for recieveing events.
+     * Schema for receiving events.
      */
-    public static final String TEST_STRING = "add";
+    public static final String STATION_ID = "StationID";
+    public static final String STATION_NAME = "StationName";
+    public static final String LATITUDE = "Latitude";
+    public static final String LONGITUDE = "Longitude";
+    public static final String DATE = "dtg";
+    public static final String OBSERVED_VALUE = "ObservedValue";
     public static final Schema SCHEMA = SchemaBuilder.struct()
             .name(DataPoint.class.getSimpleName())
-            .field(TEST_STRING, Schema.STRING_SCHEMA);
+            .field(STATION_NAME, Schema.STRING_SCHEMA)
+            .field(LATITUDE, Schema.STRING_SCHEMA)
+            .field(LONGITUDE, Schema.STRING_SCHEMA)
+            .field(DATE, Schema.STRING_SCHEMA)
+            .field(OBSERVED_VALUE, Schema.STRING_SCHEMA);
 
-    private String add;
 
-    public DataPoint(String add) {
-        this.add = add;
+    private String stationID;
+    private String stationName;
+    private String latitude;
+    private String longitude;
+    private String dtg;
+    private String observedValue;
+
+    public DataPoint(String stationID,String stationName,
+                     String latitude, String longitude,
+                     String dtg, String observedValue) {
+        this.stationID = stationID;
+        this.stationName = stationName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.dtg = dtg;
+        this.observedValue = observedValue;
     }
 
-    public String getTestString() {
-        return add;
+    public String getStationId() {
+        return stationID;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getDtg() {
+        return dtg;
+    }
+
+    public String getObservedValue() {
+        return observedValue;
     }
 
     public Struct toStruct() {
         Struct struct = new Struct(SCHEMA)
-                .put(TEST_STRING, getTestString());
+                .put(STATION_ID, getStationName())
+                .put(STATION_NAME, getStationName())
+                .put(LATITUDE, getLatitude())
+                .put(LONGITUDE, getLongitude())
+                .put(DATE, getDtg())
+                .put(OBSERVED_VALUE, getObservedValue());
+
         return struct;
     }
 }
--- io.datasearch.diseasedata.store/src/test/weatherDataAPI/api.py
+++ io.datasearch.diseasedata.store/src/test/weatherDataAPI/api.py
@@ -12,9 +12,31 @@ def hello_world():
 @app.route("/test")
 def hello_test():
    print("Executed!")
-   obj = {"dataPoints" :[{"add":"Hello_malintha"},{"add":"Hello_chanuka"},{"add":"Hello_dimuthu"}] }
-   addlist = []
-   addlist.append(obj)
+   obj = {
+   "dataPoints" :[
+   {
+       "StationId":"01CB068A"
+       "StationName":"Cinnamon Lake Side Hotel",
+       "Latitude":"6.929543",
+       "Longitude":"79.8492668",
+       "dtg":"20130526",
+       "ObservedValue":"0.6"
+   },
+   {
+      "StationId":"01GM0528"
+      "StationName":"Walpita",
+      "Latitude":"7.27",
+      "Longitude":"80",
+      "dtg":"20130526",
+      "ObservedValue":"4.2"
+   },]
+   }
+
+# ,,,,,0.6
+# 01GM0528,Walpita,7.27,80,20131206,0
+# 01KG092B,Dodangaslanda,7.57,80.5,20130130,0
+# 01JF0425,Point Pedro,9.83,80.2,20130106,0
+# 01PU313A,Marawila,7.4234916,79.8353622,20130508,4.2
    return jsonify(obj)
 
 @app.route("/weather")
