9ee5ffc5e70ea4809b0f38adadfc5044ef93eb5b
Malintha1996 <malintha1996@gmail.com>
2020-12-04 16:49:15 +0530

    Add withinRadiusMapper

--- io.datasearch.epidatafuse.core/src/main/java/io/datasearch/epidatafuse/core/fusionpipeline/model/granularitymappingmethod/MapperUtil.java
+++ io.datasearch.epidatafuse.core/src/main/java/io/datasearch/epidatafuse/core/fusionpipeline/model/granularitymappingmethod/MapperUtil.java
@@ -8,9 +8,11 @@ import java.util.Map;
  */
 public class MapperUtil {
     private static final Map<String, Map<String, Object>> MAPPERS = new HashMap<>();
+
     static {
         MAPPERS.put(NearestMapper.MAPPER_NAME, NearestMapper.getArguments());
         MAPPERS.put(ContainMapper.MAPPER_NAME, ContainMapper.getArguments());
+        MAPPERS.put(WithinRadiusMapper.MAPPER_NAME, ContainMapper.getArguments());
     }
 
     public static Map<String, Map<String, Object>> getMAPPERS() {
